e23aecbb955fd2858473303b3a7f7dcb1c2ce1ce
samantha hellen da cunha lima <samanthahcl@academicos.com>
2025-05-26 21:27:56 -0300

    aula

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import csv
+
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -6,4 +8,39 @@ app = Flask(__name__)
 def ola():
     #return '<h1> Olá, mundo! </h1>'
     return render_template('index.html')
+
+@app.route('/sobre_equipe')
+def sobre_equipe():
+    return render_template('sobre_equipe.html')
+
+@app.route('/glossario')
+def glossario():
+
+    glossario_de_termos = []
+
+    with open('bd_glossario.csv','r', newline='', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo, delimiter=';')
+        for linha in reader:
+            glossario_de_termos.append(linha)
+    # A linha abaixo foi desindentada e corrigida
+    return render_template('glossario.html', glossario=glossario_de_termos)
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('novo_termo.html')
+
+@app.route('/criar_termo', methods=['POST'])
+def criar_termo():
+
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open('bd_glossario.csv','a', newline='', encoding='utf-8') as arquivo:
+        writer = csv.writer(arquivo, delimiter=';')
+        writer.writerow([termo, definicao])
+
+    return redirect(url_for('glossario'))
+
+
+
 app.run()
